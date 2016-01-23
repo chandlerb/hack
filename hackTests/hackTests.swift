ef95b168bd538206392b4658145d0159259e1143
Chandler Brown <chandlerbrown@dhcp-10-134-247-20.uawifi.arizona.edu>
2016-01-23 13:20:03 -0700

    Initial Commit

---
+++ hackTests/hackTests.swift
@@ -0,0 +1,36 @@
+//
+//  hackTests.swift
+//  hackTests
+//
+//  Created by Chandler Brown on 1/23/16.
+//  Copyright © 2016 Chandler Brown. All rights reserved.
+//
+
+import XCTest
+@testable import hack
+
+class hackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
